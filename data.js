5aff89b97374e001d2c53715a8486308c7cf7db7
dboon4 <116426219+dboon4@users.noreply.github.com>
2025-02-14 12:02:23 +0800

    initial

---
+++ data.js
@@ -0,0 +1,625 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-001--jalan-sekama",
+      "name": "001- Jalan Sekama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.4330661783315284,
+        "pitch": 0.15217720578372962,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6273660143459114,
+          "pitch": 0.19681074481747807,
+          "rotation": 1.5707963267948966,
+          "target": "1-002-jalan-kueh-siak-hong-intersection"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-002-jalan-kueh-siak-hong-intersection",
+      "name": "002-Jalan Kueh Siak Hong Intersection",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3717691943287402,
+          "pitch": 0.23848110424159685,
+          "rotation": 0,
+          "target": "2-003-ky-cafe"
+        },
+        {
+          "yaw": 1.4200588696693064,
+          "pitch": 0.22861735069314548,
+          "rotation": 0,
+          "target": "0-001--jalan-sekama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-003-ky-cafe",
+      "name": "003-K.Y Cafe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.9397965874615863,
+        "pitch": -0.0482603289302066,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1662585277935573,
+          "pitch": 0.10892116506902383,
+          "rotation": 0,
+          "target": "1-002-jalan-kueh-siak-hong-intersection"
+        },
+        {
+          "yaw": 0.7214963878472105,
+          "pitch": 0.0654202380529334,
+          "rotation": 0,
+          "target": "0-001--jalan-sekama"
+        },
+        {
+          "yaw": -1.9582867917130802,
+          "pitch": 0.0861970966170702,
+          "rotation": 0,
+          "target": "3-004-crossroads-1"
+        },
+        {
+          "yaw": -1.5384364393654977,
+          "pitch": 0.09393050740987974,
+          "rotation": 0,
+          "target": "12-013--behind-ksh-hawker-stall-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-004-crossroads-1",
+      "name": "004-Crossroads 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.3865273393387803,
+        "pitch": 0.16717007406486317,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2089311673821488,
+          "pitch": 0.13147991431071482,
+          "rotation": 0,
+          "target": "12-013--behind-ksh-hawker-stall-"
+        },
+        {
+          "yaw": -0.06770048650238181,
+          "pitch": 0.06406519917023701,
+          "rotation": 0,
+          "target": "2-003-ky-cafe"
+        },
+        {
+          "yaw": -3.097482371297424,
+          "pitch": 0.18927049628499226,
+          "rotation": 0,
+          "target": "4-005-hong-yan"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-005-hong-yan",
+      "name": "005-Hong Yan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.4599874981449084,
+        "pitch": 0.1398124454900973,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6204147662080679,
+          "pitch": 0.1155758536770417,
+          "rotation": 0,
+          "target": "10-011-ksh-hawker-stall-middle"
+        },
+        {
+          "yaw": -1.7218855734986178,
+          "pitch": 0.13503397568423559,
+          "rotation": 0,
+          "target": "5-006-crossroad-2"
+        },
+        {
+          "yaw": 1.449503135435843,
+          "pitch": 0.1584495776984607,
+          "rotation": 0,
+          "target": "3-004-crossroads-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-006-crossroad-2",
+      "name": "006-Crossroad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6140723267602173,
+          "pitch": 0.4458804499630684,
+          "rotation": 0,
+          "target": "6-007-jalan-ksh-middle-line"
+        },
+        {
+          "yaw": 1.005468049890471,
+          "pitch": 0.16078274152768302,
+          "rotation": 0,
+          "target": "4-005-hong-yan"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-007-jalan-ksh-middle-line",
+      "name": "007-Jalan KSH Middle line",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.1078244190631246,
+        "pitch": 0.10621491348450363,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9110710418441617,
+          "pitch": 0.2001622811276924,
+          "rotation": 0,
+          "target": "7-008-lebuh-sekama"
+        },
+        {
+          "yaw": 0.8357665596476398,
+          "pitch": 0.368374689375786,
+          "rotation": 0,
+          "target": "5-006-crossroad-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-008-lebuh-sekama",
+      "name": "008-Lebuh Sekama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.5167092852641417,
+        "pitch": 0.07507162278031743,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6249538341184202,
+          "pitch": 0.12230959422686993,
+          "rotation": 0,
+          "target": "9-010-ksh-hawker-stall-start"
+        },
+        {
+          "yaw": -1.5046527238971166,
+          "pitch": 0.17818430765001025,
+          "rotation": 0,
+          "target": "8-009-new-ipoh-bbq"
+        },
+        {
+          "yaw": 0.20321831577058447,
+          "pitch": 0.14475623802302806,
+          "rotation": 0,
+          "target": "6-007-jalan-ksh-middle-line"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-009-new-ipoh-bbq",
+      "name": "009-New Ipoh BBQ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5198742496924815,
+          "pitch": 0.20810900756807627,
+          "rotation": 0,
+          "target": "7-008-lebuh-sekama"
+        },
+        {
+          "yaw": 1.0506486157754793,
+          "pitch": 0.1960063611174796,
+          "rotation": 0,
+          "target": "9-010-ksh-hawker-stall-start"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-010-ksh-hawker-stall-start",
+      "name": "010-KSH Hawker Stall Start",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.8960442907101482,
+        "pitch": 0.40218621734029014,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5913531230667299,
+          "pitch": 0.13289807528230568,
+          "rotation": 0,
+          "target": "8-009-new-ipoh-bbq"
+        },
+        {
+          "yaw": -0.43062910580994185,
+          "pitch": 0.15095712605767453,
+          "rotation": 0,
+          "target": "7-008-lebuh-sekama"
+        },
+        {
+          "yaw": 2.7966973145827394,
+          "pitch": 0.2729255179473551,
+          "rotation": 0,
+          "target": "10-011-ksh-hawker-stall-middle"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-011-ksh-hawker-stall-middle",
+      "name": "011-KSH Hawker Stall Middle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.8398462655760035,
+        "pitch": 0.15720133058235852,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3816801428268874,
+          "pitch": 0.11399289090634035,
+          "rotation": 0,
+          "target": "4-005-hong-yan"
+        },
+        {
+          "yaw": 0.2588932416573684,
+          "pitch": 0.2801341911648585,
+          "rotation": 0,
+          "target": "9-010-ksh-hawker-stall-start"
+        },
+        {
+          "yaw": -2.8776076889070588,
+          "pitch": 0.18428817224668848,
+          "rotation": 0,
+          "target": "11-012-ksh-hawker-stall-end"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-012-ksh-hawker-stall-end",
+      "name": "012-KSH Hawker Stall End",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.9554807812059884,
+        "pitch": 0.15163433360274503,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.925066067137613,
+          "pitch": 0.16955084573833545,
+          "rotation": 0,
+          "target": "12-013--behind-ksh-hawker-stall-"
+        },
+        {
+          "yaw": -0.41571214436397064,
+          "pitch": 0.14842563256668484,
+          "rotation": 0,
+          "target": "10-011-ksh-hawker-stall-middle"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-013--behind-ksh-hawker-stall-",
+      "name": "013- Behind KSH Hawker Stall ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.1955710175423953,
+        "pitch": 0.2636670673506387,
+        "fov": 1.2674161024634862
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25397446928914924,
+          "pitch": 0.053907626408872034,
+          "rotation": 4.71238898038469,
+          "target": "2-003-ky-cafe"
+        },
+        {
+          "yaw": 1.5850374216914735,
+          "pitch": 0.09272399859826663,
+          "rotation": 0,
+          "target": "3-004-crossroads-1"
+        },
+        {
+          "yaw": 2.8673906690015194,
+          "pitch": 0.2362534955538962,
+          "rotation": 0,
+          "target": "11-012-ksh-hawker-stall-end"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Kueh Siak Hong Hawker Stall",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
